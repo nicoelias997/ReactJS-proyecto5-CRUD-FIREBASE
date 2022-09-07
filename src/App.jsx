843c248ef2946bd8dde0e9c0f796cb60a4306720
nicoelias997 <nicoelias997@gmail.com>
2022-09-07 11:02:04 +0200

    Agregue maquetado para que se vean las tareas, e importe lo de firestore y como incorporarlo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
+import { async } from "@firebase/util";
 import React from "react";
+import {firebase} from "./firebase"
 
 function App() {
+  
+  const [tareas, setTareas] = React.useState([])
+
+  React.useEffect(() => {
+  
+      const obtenerDatos = async () => {
+          const db = firebase.firestore()
+          try {
+              const data = await db.collection('tareas').get()
+              const arrayData = data.docs.map(doc => ({id: doc.id, ...doc.data()}))
+              console.log(arrayData) 
+              setTareas(arrayData)     
+          } catch (error) {
+              console.log(error)
+          }
+      }
+      obtenerDatos()
+  
+  }, [])
+
   return (
-    <div className="container mt-5">
-      <h1>Probando FIREBASE</h1>
+
+    <div className="container mb-2">
+    <div className="row">
+        <div className="col-md-6">
+            <h3>Lista de Tareas</h3>
+            <ul className="list-group">
+            {
+                tareas.map(item => (
+                <li className="list-group-item" key={item.id}>
+                  <span>{item.name}</span>
+                    <button 
+                        className="btn btn-danger btn-sm float-end"
+                    >
+                        Eliminar
+                    </button>
+                    <button 
+                        className="btn btn-warning btn-sm float-end me-2"
+                    >
+                        Editar
+                    </button>
+                </li>
+                ))
+            }
+            </ul>
+        </div>
+        <div className="col-md-6">
+            <h3>Formulario</h3>
+        </div>
     </div>
+</div>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
-import firebase from "firebase/app"
-import "firebase/firestore"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBO3COeWxbxDTIiQW6RTCLYpBZxKyo1XHw",
